617b0048f1227de8a2b0733328103f9da57be032
Tanay Tapanshu <tanaytapanshu.com>
2020-09-08 01:20:29 +0530

    Removed Unwanted files , added firebase.js and header,js

--- youtube-clone-app/src/App.js
+++ youtube-clone-app/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Hi we are making a Youtube clone🚀</h1>
       </header>
     </div>
+    /* Header -> <Header /> */
+  /* Sidebar -> <Sidebar /> */
+  /* Recommended -> <Recommend /> */
+
+
   );
 }
 
---
+++ youtube-clone-app/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAXQPnkgiaK7_uXLiXCUdzpf80oMX6B2ro",
+    authDomain: "clone-56d99.firebaseapp.com",
+    databaseURL: "https://clone-56d99.firebaseio.com",
+    projectId: "clone-56d99",
+    storageBucket: "clone-56d99.appspot.com",
+    messagingSenderId: "525741652716",
+    appId: "1:525741652716:web:85e00852c31d053209c3c9",
+    measurementId: "G-6WGE17YH4K"
+};
